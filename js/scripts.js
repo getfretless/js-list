2bbf4f438f0609d655ed4992b64b8d9a1c2eb1f1
Dave Strus <dave@davestrus.com>
2014-12-11 16:53:05 -0500

    Improvements

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(function(){
   $("form#new_task").submit(function(event){
     event.preventDefault();
     var deleteButton = "<button class='delete btn btn-sm btn-danger'><i class='fa fa-trash-o'></i></button>";
@@ -17,12 +17,15 @@ $(document).ready(function(){
   });
 
   $(".tasks").on("click", "button.edit", function (){
-    var editTaskForm = "<form class='edit_task'><input type='text' name='edit_task_name'></form>";
-    var originalTask = $(this).parent().val();
+    var value = $(this).closest('.task').find('label').text().trim();
+    var editTaskForm = "<form class='edit_task form-inline'><input type='text' class='edit_task_name form-control' value='" + value + "'> <input type='submit' value='Save' class='btn btn-sm btn-default'></form>";
+
     var deleteButton = "<button class='delete btn btn-sm btn-danger'><i class='fa fa-trash-o'></i></button>";
     var editButton = "<button class='edit btn btn-sm btn-success'>Edit</button>";
     var buttons = "<div class='btn-group'>" + deleteButton + editButton + "</div>";
+
     $(this).closest("div.task").replaceWith(editTaskForm);
+
     $("form.edit_task ").on("submit", function(){
       event.preventDefault();
       var checkBox = "<label><input type='checkbox'></label>";
