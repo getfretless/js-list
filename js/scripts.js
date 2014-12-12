670b5befe2b76173810d14da4bf511a2efd8f456
Dave Strus <dave@davestrus.com>
2014-12-11 21:05:11 -0500

    Major refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,66 @@
-$(function(){
-  $("form#new_task").submit(function(event){
-    event.preventDefault();
-    var deleteButton = "<button class='delete btn btn-sm btn-danger'><i class='fa fa-trash-o'></i></button>";
-    var editButton = "<button class='edit btn btn-sm btn-success'>Edit</button>";
-    var buttons = "<div class='btn-group'>" + deleteButton + editButton + "</div>";
-    var checkBox = "<input type='checkbox'>";
-    $(".tasks").append("<li class='list-group-item'>" +
-      "<div class='task'><label class='checkbox-inline'>" + checkBox + ' ' +
-      $("#task_name").val() + '</label>' + buttons + "</div></li>");
-    $("#task_name").val('');
+var ElevenList = function() {
+  function ElevenList() {
+    var app = this;
+    var tasks = [];
+    var setupNewTaskForm = function() {
+      $('form#new_task').submit(function(event) {
+        event.preventDefault();
+        $('.tasks').append(listItem($('#task_name').val()));
+      });
+    }();
+    var setupEditTaskForm = function() {
+      $('.tasks').on('submit', 'form.edit_task', function(event) {
+        event.preventDefault();
+        var form = $(event.currentTarget);
+        value = form.find('input.edit_task_name').val();
+        form.closest('.list-group-item').replaceWith(listItem(value));
+      });
+      $('.tasks').on('click', 'button.cancel', function(event) {
+        event.preventDefault();
+        var form = $(event.currentTarget).closest('form');
+        value = form.find('input.original_value').val();
+        form.closest('.list-group-item').replaceWith(listItem(value));
+      });
+    }();
+    var setupDeleteButtons = function() {
+      $('.tasks').on('click', 'button.delete', function(){
+        $(this).closest('li').remove();
+      });
+    }();
+    var setupEditButtons = function() {
+      $('.tasks').on('click', 'button.edit', function (event) {
+        event.preventDefault();
+        var newForm = editTaskForm(this);
+        $(this).closest('div.task').replaceWith(newForm);
+        newForm.find('input.edit_task_name').select();
+      });
+    }();
+    var listItem = function(value) {
+      return '<li class="list-group-item">' +
+          '<div class="task">' +
+            '<label class="checkbox-inline">' +
+              '<input type="checkbox"> ' +
+              value +
+            '</label>' +
+            '<div class="btn-group">' +
+              '<button class="delete btn btn-sm btn-danger">' +
+                '<i class="fa fa-trash-o"></i>' +
+              '</button>' +
+              '<button class="edit btn btn-sm btn-success">' +
+                'Edit' +
+              '</button>' +
+            '</div>' +
+          '</div>' +
+        '</li>';
+    };
+    var editTaskForm = function(element) {
+      var value = $(element).closest('.task').find('label').text().trim();
+      var form = $('form.edit_task.hidden').clone();
+      form.find('input.edit_task_name, input.original_value').val(value);
+      return form.removeClass('hidden');
+    };
+  }
+  return ElevenList;
+}();
 
-  });
-
-  $(".tasks").on("click", "button.delete", function(){
-    $(this).closest("li").remove();
-  });
-
-  $(".tasks").on("click", "button.edit", function (){
-    var value = $(this).closest('.task').find('label').text().trim();
-    var editTaskForm = "<form class='edit_task form-inline'><input type='text' class='edit_task_name form-control' value='" + value + "'> <input type='submit' value='Save' class='btn btn-sm btn-default'></form>";
-
-    var deleteButton = "<button class='delete btn btn-sm btn-danger'><i class='fa fa-trash-o'></i></button>";
-    var editButton = "<button class='edit btn btn-sm btn-success'>Edit</button>";
-    var buttons = "<div class='btn-group'>" + deleteButton + editButton + "</div>";
-
-    $(this).closest("div.task").replaceWith(editTaskForm);
-
-    $("form.edit_task ").on("submit", function(){
-      event.preventDefault();
-      var checkBox = "<label><input type='checkbox'></label>";
-      $(this).replaceWith("<div>" + $(".edit_task_name").val() + buttons + "</div>");
-    });
-  });
-
-  $(".tasks").on("click", ":checkbox", function (){
-    $(this).closest("li").toggleClass("completed");
-  });
-});
+elevenlist = new ElevenList();
