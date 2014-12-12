56e536848079b804dab60f15be7d8345d3248530
Dave Strus <dave@davestrus.com>
2014-12-12 00:42:35 -0500

    Serialize tasks.

--- js/storage.js
+++ js/storage.js
@@ -1,11 +1,25 @@
 var ElevenList = function() {
   function ElevenList() {
-    var app = this;
-    $('.tasks').sortable();
+    var _this = this;
+    this.tasks = {};
+    this.counter = 0;
+    $('.tasks').sortable({
+      update: function() { updatePositions(); }
+    });
+
     var setupNewTaskForm = function() {
       $('form#new_task').submit(function(event) {
         event.preventDefault();
-        $('.tasks').append(listItem($('#task_name').val()));
+        _this.counter ++;
+        var id = _this.counter.toString()
+        var task = {
+          id: id,
+          position: _this.counter,
+          name: $('#task_name').val(),
+          completed: false
+        };
+        _this.tasks[id] = task;
+        $('.tasks').append(listItem(task));
         $('#task_name').val('');
       });
     }();
@@ -14,7 +28,10 @@ var ElevenList = function() {
         event.preventDefault();
         var form = $(event.currentTarget);
         value = form.find('input.task_name').val();
-        form.closest('li').replaceWith(listItem(value));
+        var id = form.closest('li').attr('id')
+        id = id.substring(id.lastIndexOf('_') + 1);
+        _this.tasks[id].name = value;
+        form.closest('li').replaceWith(listItem(_this.tasks[id]));
       });
       $('.tasks').on('click', 'button.cancel', function(event) {
         event.preventDefault();
@@ -32,21 +49,24 @@ var ElevenList = function() {
       $('.tasks').on('click', 'button.edit', function (event) {
         event.preventDefault();
         var newForm = editTaskForm(this);
-        $(this).closest('div.task').replaceWith(newForm);
+        $(this).closest('div.task').html(newForm);
         newForm.find('input.task_name').select();
       });
     }();
     var setupCheckboxes = function() {
-      $('.tasks').on('click', ':checkbox', function (){
+      $('.tasks').on('click', ':checkbox', function () {
+        var id = $(this).closest('li').attr('id')
+        id = id.substring(id.lastIndexOf('_') + 1);
         $(this).closest('li').toggleClass('completed');
+        _this.tasks[id].completed = $(this).closest('li').hasClass('completed');
       });
     }()
-    var listItem = function(value) {
-      return '<li class="list-group-item">' +
+    var listItem = function(task) {
+      return '<li class="list-group-item" id="task_' + task.id + '">' +
           '<div class="task">' +
-            '<label class="checkbox-inline">' +
+            '<label class="checkbox-inline"' + (task.completed ? ' checked>' : '>') +
               '<input type="checkbox"> ' +
-              value +
+              task.name +
             '</label>' +
             '<div class="btn-group">' +
               '<button class="delete btn btn-sm btn-danger">' +
@@ -64,6 +84,14 @@ var ElevenList = function() {
       var form = $('form.edit_task.hidden').clone();
       form.find('input.task_name, input.original_value').val(value);
       return form.removeClass('hidden');
+    };
+    var updatePositions = function() {
+      var tasks = $('.tasks').sortable('toArray');
+      var i, id;
+      for (i=0; i < tasks.length; i++) {
+        id = tasks[i].substring(tasks[i].lastIndexOf('_') + 1);
+        _this.tasks[id].position = i + 1;
+      }
     };
   }
   return ElevenList;
